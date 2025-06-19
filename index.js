a350c56f3aa4804d74c69efc264d3fd895df9e40
fadhiil ysuuf <fadhiilyus07@gmail.com>
2025-06-19 09:54:35 +0700

    fix: add root route

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Nutech Integrasi API!');
+  res.send('🚀 Nutech API is running!');
 });
 
 app.listen(PORT, () => {
